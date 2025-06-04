b2316b46e62583a86efd959fae33928749e1b93d
sachin-008 <sachin.asingh18@gmail.com>
2025-06-04 14:13:31 +0530

    Remove all files from remote repo

--- modules/analyzer.py
+++
@@ -1,8 +0,0 @@
-# analyzer.py
-def generate_summary(df):
-    return {
-        "shape": df.shape,
-        "columns": list(df.columns),
-        "missing": df.isnull().sum().to_dict(),
-        "describe": df.describe(include='all').to_dict()
-    }
--- modules/data_cleaner.py
+++
@@ -1,5 +0,0 @@
-# data_cleaner.py
-def clean_data(df):
-    df = df.dropna(how='all')  # Drop empty rows
-    df.columns = df.columns.str.strip()
-    return df
