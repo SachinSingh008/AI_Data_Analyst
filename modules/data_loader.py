80d48ca1e1fb74975e113c47ded0658fb3f34003
Sachin Singh <152496895+SachinSingh008@users.noreply.github.com>
2025-06-04 14:04:24 +0530

    Delete modules directory

--- modules/data_loader.py
+++
@@ -1,8 +0,0 @@
-# data_loader.py
-import pandas as pd
-
-def load_data(file_path: str):
-    try:
-        return pd.read_csv(file_path)
-    except Exception as e:
-        raise ValueError(f"Error loading file: {e}")
