9c062b70fcdbc63683377f513250b7c49df6bdd9
sachin-008 <sachin.asingh18@gmail.com>
2025-07-10 15:52:14 +0530

    edited changes

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,8 +1,17 @@
-# data_loader.py
 import pandas as pd
 
-def load_data(file_path: str):
+def load_data(uploaded_file):
     try:
-        return pd.read_csv(file_path)
+        if uploaded_file.name.endswith(".csv"):
+            try:
+                return pd.read_csv(uploaded_file)
+            except UnicodeDecodeError:
+                return pd.read_csv(uploaded_file, encoding="ISO-8859-1")
+        elif uploaded_file.name.endswith(".xlsx"):
+            return pd.read_excel(uploaded_file)
+        elif uploaded_file.name.endswith(".json"):
+            return pd.read_json(uploaded_file)
+        else:
+            raise ValueError("Unsupported file format")
     except Exception as e:
         raise ValueError(f"Error loading file: {e}")
